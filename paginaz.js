33bdf97c64a95c53aa75b999c44143ddec1c2f66
Yeifer Orozco <yeiferorozco@outlook.com>
2025-03-30 03:28:43 -0500

    Update paginaz.js

--- paginaz.js
+++ paginaz.js
@@ -140,7 +140,7 @@ function redirectSearch(pageNum) {
 
     let script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = `${home_page}feeds/posts/summary?q=${getSearchQuery()}&start-index=${jsonstart}&max-results=1&alt=json-in-script&callback=finddatepost`;
+    script.src = `${home_page}feeds/posts/summary?q=${getSearchQuery()}&start-index=${jsonstart}&max-results=12&alt=json-in-script&callback=finddatepost`;
 
     document.getElementsByTagName("head")[0].appendChild(script);
 }
