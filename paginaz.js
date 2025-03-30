5d4bbf96287f6bdd2c4f965eea350f0f922fb818
Yeifer Orozco <yeiferorozco@outlook.com>
2025-03-30 00:48:53 -0500

    Update paginaz.js

--- paginaz.js
+++ paginaz.js
@@ -105,7 +105,7 @@ function bloggerpage() {
 } else if (activePage.includes("?q=")) {
     type = "search";
     searchQuery = activePage.split("?q=")[1].split("&")[0]; // 🔥 Extrae el término de búsqueda
-    document.write(`<script src="${home_page}feeds/posts/summary?q=${searchQuery}&alt=json-in-script&callback=paginationall&max-results=1"></script>`);
+    document.write(`<script src="${home_page}feeds/posts/summary?q=${searchQuery}&alt=json-in-script&callback=paginationall&max-results=1000"></script>`);
 } else {
         type = "page";
     }
