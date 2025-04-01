4edd4243ff4c1163724e204f3ca2de2d3ae64e3c
Yeifer Orozco <yeiferorozco@outlook.com>
2025-04-01 00:03:55 -0500

    Update paginaz.js

--- paginaz.js
+++ paginaz.js
@@ -89,29 +89,26 @@ function paginationall(data) {
 }
 
 function bloggerpage() {
-    searchQuery = getSearchQuery();
     let activePage = window.location.href;
 
-    if (activePage.includes("/search/label/")) {
-        type = "label";
-        lblname1 = activePage.split("/search/label/")[1].split("?")[0];
-    } else if (searchQuery) {
-        type = "search";
-    } else {
+    // Si estamos en la portada, ajustamos la URL de la solicitud para obtener todas las publicaciones
+    if (!activePage.includes("?q=") && !activePage.includes(".html") && activePage.indexOf("/search/label/") === -1) {
         type = "page";
+        currentPage = activePage.includes("#PageNo=") ? parseInt(activePage.substring(activePage.indexOf("#PageNo=") + 8), 10) : 1;
+
+        // Aseguramos de que estamos obteniendo el total de publicaciones y no solo una
+        document.write(`<script src="${home_page}feeds/posts/summary?max-results=9999&alt=json-in-script&callback=paginationall"></script>`);
+    } else {
+        // Si no estamos en la portada, tratamos como una búsqueda o etiqueta
+        type = "label";
+        if (!activePage.includes("&max-results=")) {
+            itemsPerPage = 12;
+        }
+        currentPage = activePage.includes("#PageNo=") ? parseInt(activePage.substring(activePage.indexOf("#PageNo=") + 8), 10) : 1;
+
+        // Petición para obtener las entradas de la etiqueta
+        document.write(`<script src="${home_page}feeds/posts/summary/-/${lblname1}?alt=json-in-script&callback=paginationall&max-results=9999"></script>`);
     }
-
-    currentPage = activePage.includes("#PageNo=") 
-        ? parseInt(activePage.split("#PageNo=")[1]) 
-        : 1;
-
-    let scriptUrl = type === "search"
-        ? `${home_page}feeds/posts/summary?max-results=1&alt=json-in-script&callback=paginationall`
-        : `${home_page}feeds/posts/summary/-/${lblname1}?max-results=1&alt=json-in-script&callback=paginationall`;
-
-    let script = document.createElement("script");
-    script.src = scriptUrl;
-    document.body.appendChild(script);
 }
 
 document.addEventListener("DOMContentLoaded", bloggerpage);
