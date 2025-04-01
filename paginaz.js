24bbbcc8e4920c74c84c6a09ca100b133879ea8a
Yeifer Orozco <yeiferorozco@outlook.com>
2025-04-01 03:02:38 -0500

    Update paginaz.js

--- paginaz.js
+++ paginaz.js
@@ -1,3 +1,27 @@
+/*
+ * Paginación para Blogger - Creado por Yeifer Orozco
+ * Copyright © 2025 Yeifer Orozco
+ * 
+ * Licencia MIT
+ * 
+ * Por la presente se concede permiso, libre de cargos, a cualquier persona que obtenga una copia
+ * de este software y de los archivos de documentación asociados (el "Software"), para utilizar
+ * el Software sin restricción, incluyendo sin limitación los derechos a usar, copiar, modificar,
+ * fusionar, publicar, distribuir, sublicenciar, y/o vender copias del Software, y a permitir a
+ * las personas a las que se les proporcione el Software a hacer lo mismo, sujeto a las siguientes
+ * condiciones:
+ * 
+ * El aviso de copyright anterior y este aviso de permiso deberán ser incluidos en todas las copias
+ * o porciones sustanciales del Software.
+ * 
+ * EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O IMPLÍCITA,
+ * INCLUYENDO PERO NO LIMITADO A LAS GARANTÍAS DE COMERCIABILIDAD, APTITUD PARA UN PROPÓSITO
+ * PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS AUTORES O TITULARES DEL COPYRIGHT SERÁN
+ * RESPONSABLES POR NINGUNA RECLAMACIÓN, DAÑOS U OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN
+ * DE CONTRATO, AGRAVIO O DE OTRO MODO, QUE SURJA DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE
+ * O EL USO U OTROS TRATOS EN EL SOFTWARE.
+ */
+
 // Parámetros globales (estas variables se definirán en la plantilla)
 var currentPage, searchQuery, lastPostDate = null, type, lblname1, nopage;
 
